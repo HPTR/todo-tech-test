cd66cc8ce268e0046385f0d21c5149b0a843a683
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-16 09:56:17 +0100

    Import main app components and pass props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+import ResetButton from './components/ResetButton/ResetButton'
+import AddTodo from './components/AddTodo/AddTodo'
+import TodoList from './components/TodoList/TodoList'
 
 function App() {
 
-
+  const [todos, setTodos] = useState([])
 
   return (
     <div className="App">
       <h1>My Todos</h1>
-      <ResetButton />
-      <AddTodo />
-      <TodoList />
+      <ResetButton resetTodos={setTodos} />
+      <AddTodo addTodo={setTodos} />
+      <TodoList todos={todos} />
     </div>
   );
 }
